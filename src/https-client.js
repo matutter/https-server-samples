edd30d48820762f8ddfe6983827f0070ffadb5a0
matutter <matutter4@gmail.com>
2018-02-02 22:10:14 -0500

    added nodejs sample https-client/server

---
+++ src/https-client.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var https = require('https');
+
+const cert_dir = 'test/client/'
+
+var options = { 
+    hostname: 'localhost', 
+    port: 4433, 
+    path: '/', 
+    method: 'GET', 
+    key: fs.readFileSync(cert_dir+'client-key.pem'), 
+    cert: fs.readFileSync(cert_dir+'client-crt.pem'), 
+    ca: fs.readFileSync(cert_dir+'ca-crt.pem')
+};
+
+var req = https.request(options, function(res) { 
+    res.on('data', function(data) { 
+        process.stdout.write(data); 
+    }); 
+}); 
+
+req.end(); 
+
+req.on('error', function(e) { 
+    console.error(e); 
+});
---
+++ src/https-server.js
@@ -0,0 +1,24 @@
+var fs = require('fs'); 
+var https = require('https'); 
+
+const cert_dir = 'test/server/';
+
+var options = { 
+    key: fs.readFileSync(cert_dir+'server-key.pem'), 
+    cert: fs.readFileSync(cert_dir+'server-crt.pem'), 
+    ca: fs.readFileSync(cert_dir+'ca-crt.pem'), 
+    requestCert: true, 
+    rejectUnauthorized: true
+}; 
+
+https.createServer(options, function (req, res) { 
+    console.log(new Date()+' '+ 
+        req.connection.remoteAddress+' '+ 
+        req.socket.getPeerCertificate().subject.CN+' '+ 
+        req.method+' '+req.url); 
+    res.writeHead(200);
+    let cn = req.socket.getPeerCertificate().subject.CN;
+    res.end(`hello ${cn}\n`); 
+}).listen(4433, () => {
+    console.log('server running');
+});
